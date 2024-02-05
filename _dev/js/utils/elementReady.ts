60725b5de7a022f71b40f26b76140ccf2c934545
Kamil Doroszewicz <kamil.doroszewicz@tpay.com>
2024-02-05 14:18:55 +0100

    fix blik init in supercheckout plugin

---
+++ _dev/js/utils/elementReady.ts
@@ -0,0 +1,31 @@
+// MIT Licensed
+// Author: jwilson8767
+
+/**
+ * Waits for an element satisfying selector to exist, then resolves promise with the element.
+ * Useful for resolving race conditions.
+ *
+ * @param selector
+ * @returns {Promise}
+ */
+export function elementReady(selector: string) {
+    return new Promise((resolve, reject) => {
+      let el = document.querySelector(selector);
+      if (el) {
+        resolve(el); 
+        return
+      }
+      new MutationObserver((mutationRecords, observer) => {
+        // Query for elements matching the specified selector
+        Array.from(document.querySelectorAll(selector)).forEach((element) => {
+          resolve(element);
+          //Once we have resolved we don't need the observer anymore.
+          observer.disconnect();
+        });
+      })
+        .observe(document.documentElement, {
+          childList: true,
+          subtree: true
+        });
+    });
+  }
