b9ad21c46f6e15396700970b74996b3f6460708e
s4ddly <110701663+s4ddly@users.noreply.github.com>
2024-04-04 14:44:26 +0200

    Update package version

--- tpay.php
+++ tpay.php
@@ -143,7 +143,7 @@ class Tpay extends PaymentModule
     {
         $this->name = 'tpay';
         $this->tab = 'payments_gateways';
-        $this->version = '1.8.5';
+        $this->version = '1.8.6';
         $this->author = 'Krajowy Integrator Płatności S.A.';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = [
