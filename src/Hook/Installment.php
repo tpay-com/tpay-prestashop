356f85ad5f0eccae00c2691f6ed5d6bf76bd0585
kGablo <krzysztof.gablo@tpay.com>
2024-09-11 14:05:45 +0200

    Fix admin notification

--- src/Hook/Installment.php
+++ src/Hook/Installment.php
@@ -24,7 +24,7 @@ class Installment extends AbstractHook
 {
     public const AVAILABLE_HOOKS = [
         'displayShoppingCart',
-        'displayCheckoutSummaryTop',
+        'displayPaymentTop',
         'displayProductPriceBlock'
     ];
 
