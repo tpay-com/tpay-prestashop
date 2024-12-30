4ede5994dbf485813e7f04b746a9d4037e166663
Karol Wojciechowski <kwojc@outlook.com>
2024-12-30 15:34:50 +0100

    Update tpay.php
    Version raise

--- tpay.php
+++ tpay.php
@@ -111,7 +111,7 @@ class Tpay extends PaymentModule
     {
         $this->name = 'tpay';
         $this->tab = 'payments_gateways';
-        $this->version = '1.9.7';
+        $this->version = '1.9.8';
         $this->author = 'Krajowy Integrator Płatności S.A.';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = [
