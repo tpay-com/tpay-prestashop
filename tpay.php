49fd5e047443e6533b0c639997ead751e29f7c3b
Kamil Balwierz <kamil.balwierz@tpay.com>
2025-07-07 10:38:38 +0200

    Changelog for version 1.10.5

--- tpay.php
+++ tpay.php
@@ -112,7 +112,7 @@ class Tpay extends PaymentModule
     {
         $this->name = 'tpay';
         $this->tab = 'payments_gateways';
-        $this->version = '1.10.4';
+        $this->version = '1.10.5';
         $this->author = 'Krajowy Integrator Płatności S.A.';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = [
