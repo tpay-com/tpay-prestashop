47e0517fe81f1109acb5b07c42d6b9d4e4683aea
Kamil Balwierz <kamil.balwierz@tpay.com>
2025-09-26 10:59:47 +0200

    Fix last name issue (#52)
    * Fix lastname issiue
    
    * Release 1.11.1 changelog

--- tpay.php
+++ tpay.php
@@ -112,7 +112,7 @@ class Tpay extends PaymentModule
     {
         $this->name = 'tpay';
         $this->tab = 'payments_gateways';
-        $this->version = '1.10.6';
+        $this->version = '1.11.1';
         $this->author = 'Krajowy Integrator Płatności S.A.';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = [
