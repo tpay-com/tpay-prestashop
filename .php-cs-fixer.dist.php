0f8cefd5c9a28a6d3ed4467191d4ba3fbb8eb4c2
Aleksander Kąkol <aleksander.kakol@tpay.com>
2025-12-12 11:47:24 +0100

    fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,15 +1,10 @@
 <?php
 
-require __DIR__ . '/vendor/prestashop/php-dev-tools/src/CsFixer/Config.php';
+require __DIR__.'/vendor/tpay-com/coding-standards/bootstrap.php';
 
-$config = new PrestaShop\CodingStandards\CsFixer\Config();
-
-$config
-    ->setUsingCache(true)
-    ->getFinder()
-    ->in(__DIR__)
-    ->exclude('translations')
-    ->exclude('prestashop')
-    ->exclude('vendor');
-
-return $config;
+return Tpay\CodingStandards\PhpCsFixerConfigFactory::createWithLegacyRules()
+    ->setFinder(
+        PhpCsFixer\Finder::create()->ignoreDotFiles(false)->in(__DIR__)->exclude('translations')
+            ->exclude('prestashop')
+            ->exclude('vendor')
+    );
