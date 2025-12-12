7bdfb24e47567280fdf6613f11a47b5c465b322a
Aleksander Kąkol <aleksander.kakol@tpay.com>
2025-12-12 12:05:05 +0100

    fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,7 +4,10 @@ require __DIR__.'/vendor/tpay-com/coding-standards/bootstrap.php';
 
 return Tpay\CodingStandards\PhpCsFixerConfigFactory::createWithLegacyRules()
     ->setFinder(
-        PhpCsFixer\Finder::create()->ignoreDotFiles(false)->in(__DIR__)->exclude('translations')
+        PhpCsFixer\Finder::create()
+            ->ignoreDotFiles(false)
+            ->in(__DIR__)
+            ->exclude('translations')
             ->exclude('prestashop')
             ->exclude('vendor')
     );
