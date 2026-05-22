56403140f3d7e289dfdbcad3b0aef6b75ce85a9c
Karol Wojciechowski <karol.wojciechowski@tpay.com>
2026-05-22 13:37:31 +0200

    locks

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,21 +1,9 @@
 <?php
 
-require __DIR__.'/vendor/tpay-com/coding-standards/bootstrap.php';
+$config = new PrestaShop\CodingStandards\CsFixer\Config();
 
-$config = Tpay\CodingStandards\PhpCsFixerConfigFactory::createWithLegacyRules();
+/** @var \Symfony\Component\Finder\Finder $finder */
+$finder = $config->setUsingCache(true)->getFinder();
+$finder->in(__DIR__)->exclude('vendor');
 
-return $config
-    ->setRules(
-        [
-            'multiline_string_to_heredoc' => false,
-            'single_line_comment_style' => ['comment_types' => ['hash']],
-        ] + $config->getRules()
-    )
-    ->setFinder(
-        PhpCsFixer\Finder::create()
-            ->ignoreDotFiles(false)
-            ->in(__DIR__)
-            ->exclude('translations')
-            ->exclude('prestashop')
-            ->exclude('vendor')
-    );
+return $config;
