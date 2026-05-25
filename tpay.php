4f612bfadd45b1b962fabfa67053ce579bdcd629
Karol Wojciechowski <karol.wojciechowski@tpay.com>
2026-05-25 16:56:20 +0200

    fix

--- tpay.php
+++ tpay.php
@@ -245,8 +245,8 @@ class Tpay extends PaymentModule
         }
 
         if (!parent::install() || false === (new Install(
-                $this, new InstallQueryHandler(), $this->getContext()
-            ))->install()) {
+            $this, new InstallQueryHandler(), $this->getContext()
+        ))->install()) {
             $this->_errors[] = $this->trans('Installation error', [], 'Modules.Tpay.Admin');
         }
 
@@ -337,7 +337,7 @@ class Tpay extends PaymentModule
     {
         $currency_order = new Currency($cart->id_currency);
 
-        return (bool)('PLN' == $currency_order->iso_code);
+        return (bool) ('PLN' == $currency_order->iso_code);
     }
 
     public function fetch($templatePath, $cache_id = null, $compile_id = null)
@@ -477,7 +477,7 @@ class Tpay extends PaymentModule
     {
         $clientId = Cfg::get('TPAY_CLIENT_ID');
         $secretKey = Cfg::get('TPAY_SECRET_KEY');
-        $isProduction = true !== (bool)Cfg::get('TPAY_SANDBOX');
+        $isProduction = true !== (bool) Cfg::get('TPAY_SANDBOX');
 
         if ($clientId && $secretKey) {
             try {
