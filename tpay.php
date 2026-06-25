74afba2880f0eacedaa9585a953ebdd39e194124
Karol Wojciechowski <karol.wojciechowski@tpay.com>
2026-06-25 11:14:42 +0200

    Ver change

--- tpay.php
+++ tpay.php
@@ -135,7 +135,7 @@ class Tpay extends PaymentModule
     {
         $this->name = 'tpay';
         $this->tab = 'payments_gateways';
-        $this->version = '1.14.6';
+        $this->version = '1.15.0';
         $this->author = 'Krajowy Integrator Płatności S.A.';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = [
